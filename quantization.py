2cc91e1a71b1e7af874d3280f45b249e87bf3ca5
YaoJiawei329 <56985607+YaoJiawei329@users.noreply.github.com>
2023-09-14 19:10:47 +0800

    Add files via upload

---
+++ quantization.py
@@ -0,0 +1,76 @@
+import numpy as np
+# import torch
+# import torch.nn as nn
+import util_trt
+import glob, os, cv2
+
+BATCH_SIZE = 5
+BATCH = 50
+height = 512
+width = 512
+CALIB_IMG_DIR = './popian_calib_pics/'
+onnx_model_path = './2023-8-19-popian_15.onnx'
+
+
+def preprocess(img):
+    img = cv2.resize(img, (height, width))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.transpose((2, 0, 1)).astype(np.float32)
+    img /= 255.0
+    return img
+
+
+class DataLoader:
+    def __init__(self):
+        self.index = 0
+        self.length = BATCH
+        self.batch_size = BATCH_SIZE
+        # self.img_list = [i.strip() for i in open('calib.txt').readlines()]
+        self.img_list = glob.glob(os.path.join(CALIB_IMG_DIR, "*.jpg"))
+        assert len(self.img_list) > self.batch_size * self.length, '{} must contains more than '.format(
+            CALIB_IMG_DIR) + str(self.batch_size * self.length) + ' images to calib'
+        print('found all {} images to calib.'.format(len(self.img_list)))
+        self.calibration_data = np.zeros((self.batch_size, 3, height, width), dtype=np.float32)
+
+    def reset(self):
+        self.index = 0
+
+    def next_batch(self):
+        if self.index < self.length:
+            for i in range(self.batch_size):
+                assert os.path.exists(self.img_list[i + self.index * self.batch_size]), 'not found!!'
+                img = cv2.imread(self.img_list[i + self.index * self.batch_size])
+                img = preprocess(img)
+                self.calibration_data[i] = img
+
+            self.index += 1
+
+            # example only
+            return np.ascontiguousarray(self.calibration_data, dtype=np.float32)
+        else:
+            return np.array([])
+
+    def __len__(self):
+        return self.length
+
+
+def main():
+    # onnx2trt
+    fp16_mode = False
+    int8_mode = True
+    print('*** onnx to tensorrt begin ***')
+    # calibration
+    calibration_stream = DataLoader()
+    engine_model_path = "modelInt8.engine"
+    calibration_table = 'best_calibration.cache'
+    # fixed_engine,校准产生校准表
+    engine_fixed = util_trt.get_engine(BATCH_SIZE, onnx_model_path, engine_model_path, fp16_mode=fp16_mode,
+                                       int8_mode=int8_mode, calibration_stream=calibration_stream,
+                                       calibration_table_path=calibration_table, save_engine=True)
+    print(engine_fixed)
+    assert engine_fixed, 'Brokenls engine_fixed'
+    print('*** onnx to tensorrt completed ***\n')
+
+
+if __name__ == '__main__':
+    main()
